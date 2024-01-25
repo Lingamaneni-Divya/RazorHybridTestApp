c9cdd9e5825288179cc0264ea2e58139bb2ff82c
Lingamaneni-Divya <52616361+Lingamaneni-Divya@users.noreply.github.com>
2024-01-25 14:02:07 +0530

    Create MainWindow.xaml.cs

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+// MainWindow.xaml.cs
+using System;
+using System.Windows;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        MyWpfPage myWpfPage = new MyWpfPage();
+        myWpfPage.SomethingHappened += HandleSomethingHappened;
+
+        MainFrame.Navigate(myWpfPage);
+    }
+
+    private void HandleSomethingHappened(object sender, EventArgs e)
+    {
+        MessageBox.Show("Button clicked in WPF Page!");
+    }
+
+    private async void WebView_NavigationCompleted(object sender, Microsoft.Web.WebView2.Core.CoreWebView2NavigationCompletedEventArgs e)
+    {
+        await WebView.CoreWebView2.AddScriptToExecuteOnDocumentCreatedAsync(
+            "function notifyWpfApplication() {" +
+            "  DotNet.invokeMethod('WpfApp', 'HandleSomethingHappened');" +
+            "}");
+    }
+}
