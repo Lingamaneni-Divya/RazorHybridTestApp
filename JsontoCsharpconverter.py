e46bcc13faf5eabee6396a7d037bf089cc3c5a8a
Lingamaneni-Divya <52616361+Lingamaneni-Divya@users.noreply.github.com>
2024-08-02 09:33:38 +0530

    Update JsontoCsharpconverter.py

--- JsontoCsharpconverter.py
+++ JsontoCsharpconverter.py
@@ -36,6 +36,8 @@ class ClassGenerator:
         elif isinstance(value, list):
             if value:
                 list_type, nested_class_def = self.get_csharp_type(value[0], key)
+                if isinstance(value[0], dict):
+                    nested_class_def = self.json_to_cs_class(value[0], list_type)
                 return f"List<{list_type}>", nested_class_def
             else:
                 return "List<object>", None
@@ -46,18 +48,33 @@ class ClassGenerator:
         else:
             return "string", None
 
-    def convert_json_file_to_cs(self, json_file_path, cs_file_path, class_name):
+    def convert_json_file_to_cs(self, json_file_path, cs_file_path, root_class_name):
         with open(json_file_path, 'r') as json_file:
             json_obj = json.load(json_file)
         
-        cs_classes = ""
-        if isinstance(json_obj, list):
-            cs_classes += self.json_to_cs_class(json_obj[0], class_name)
-        elif 'value' in json_obj:
-            for item in json_obj['value']:
-                cs_classes += self.json_to_cs_class(item, class_name)
+        if isinstance(json_obj, dict) and 'value' in json_obj:
+            if isinstance(json_obj['value'], list):
+                if json_obj['value'] and isinstance(json_obj['value'][0], dict):
+                    # Handle array of nested JSON objects
+                    root_class_def = f"public class {root_class_name}\n{{\n    public List<Root> Value {{ get; set; }}\n}}\n"
+                    self.generated_classes[root_class_name] = root_class_def
+                    self.json_to_cs_class(json_obj['value'][0], "Root")
+                else:
+                    # Handle array of simple JSON objects
+                    root_class_def = f"public class {root_class_name}\n{{\n    public List<object> Value {{ get; set; }}\n}}\n"
+                    self.generated_classes[root_class_name] = root_class_def
+            elif isinstance(json_obj['value'], dict):
+                # Handle single nested JSON object
+                root_class_def = f"public class {root_class_name}\n{{\n    public Root Value {{ get; set; }}\n}}\n"
+                self.generated_classes[root_class_name] = root_class_def
+                self.json_to_cs_class(json_obj['value'], "Root")
+            else:
+                # Handle simple JSON object
+                root_class_def = f"public class {root_class_name}\n{{\n    public object Value {{ get; set; }}\n}}\n"
+                self.generated_classes[root_class_name] = root_class_def
         else:
-            cs_classes += self.json_to_cs_class(json_obj, class_name)
+            # Handle case where root is a single object
+            self.json_to_cs_class(json_obj, root_class_name)
         
         with open(cs_file_path, 'w') as cs_file:
             for class_def in self.generated_classes.values():
@@ -68,7 +85,7 @@ class ClassGenerator:
 # Example usage
 json_file_path = "input.json"  # Path to your JSON file
 cs_file_path = "OutputClass.cs"  # Desired path for the generated C# class file
-class_name = "RootClass"  # Desired root class name
+root_class_name = "RootClass"  # Desired root class name
 
 generator = ClassGenerator()
-generator.convert_json_file_to_cs(json_file_path, cs_file_path, class_name)
+generator.convert_json_file_to_cs(json_file_path, cs_file_path, root_class_name)
