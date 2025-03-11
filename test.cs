e7658bf38d6800b8d5e61d482cd09af7d3b09c14
Lingamaneni-Divya <52616361+Lingamaneni-Divya@users.noreply.github.com>
2025-03-11 14:31:09 +0530

    Update test.cs

--- test.cs
+++ test.cs
@@ -1,22 +1,17 @@
-
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 using Moq;
 using Xunit;
 using IntuneMobilityViolationJob.Repository.Command.BaseRepository;
-public interface IDbConnectionFactory
-{
-    SqlConnection CreateConnection();
-}
 
 public class CommandRepositoryTests
 {
     private readonly Mock<IConfiguration> _mockConfig;
-    private readonly Mock<IDbConnectionFactory> _mockConnectionFactory;
     private readonly Mock<SqlConnection> _mockConnection;
     private readonly Mock<SqlCommand> _mockCommand;
     private readonly Mock<SqlTransaction> _mockTransaction;
@@ -27,22 +22,18 @@ public class CommandRepositoryTests
         _mockConfig = new Mock<IConfiguration>();
         _mockConfig.Setup(c => c["ConnectionStrings:MobilityViolenceWriteDB"]).Returns("FakeConnectionString");
 
-        _mockConnectionFactory = new Mock<IDbConnectionFactory>();
         _mockConnection = new Mock<SqlConnection>();
         _mockCommand = new Mock<SqlCommand>();
         _mockTransaction = new Mock<SqlTransaction>();
 
-        // Set up factory to return the fake connection
-        _mockConnectionFactory.Setup(f => f.CreateConnection()).Returns(_mockConnection.Object);
-
-        // Mock open connection behavior
+        // Mock connection behavior
         _mockConnection.Setup(c => c.OpenAsync(It.IsAny<System.Threading.CancellationToken>())).Returns(Task.CompletedTask);
         _mockConnection.Setup(c => c.BeginTransaction()).Returns(_mockTransaction.Object);
 
         // Mock command behavior
         _mockCommand.Setup(c => c.ExecuteNonQueryAsync(It.IsAny<System.Threading.CancellationToken>())).ReturnsAsync(1);
+        _mockConnection.Setup(c => c.CreateCommand()).Returns(_mockCommand.Object);
 
-        // Create instance of CommandRepository with mocked factory
         _commandRepository = new CommandRepository(_mockConfig.Object);
     }
 
@@ -52,7 +43,6 @@ public class CommandRepositoryTests
         // Arrange
         string commandText = "INSERT INTO SampleTable VALUES ('TestName')";
 
-        _mockConnection.Setup(c => c.CreateCommand()).Returns(_mockCommand.Object);
         _mockCommand.SetupSet(c => c.Connection = _mockConnection.Object);
         _mockCommand.SetupSet(c => c.Transaction = _mockTransaction.Object);
         _mockCommand.SetupSet(c => c.CommandText = commandText);
@@ -71,9 +61,12 @@ public class CommandRepositoryTests
     {
         // Arrange
         string commandText = "INSERT INTO SampleTable VALUES ('TestName')";
-        _mockCommand.Setup(c => c.ExecuteNonQueryAsync(It.IsAny<System.Threading.CancellationToken>())).ThrowsAsync(new SqlException());
+        
+        // Generate a fake SqlException using reflection
+        var sqlException = GenerateFakeSqlException();
 
-        _mockConnection.Setup(c => c.CreateCommand()).Returns(_mockCommand.Object);
+        _mockCommand.Setup(c => c.ExecuteNonQueryAsync(It.IsAny<System.Threading.CancellationToken>())).ThrowsAsync(sqlException);
+
         _mockCommand.SetupSet(c => c.Connection = _mockConnection.Object);
         _mockCommand.SetupSet(c => c.Transaction = _mockTransaction.Object);
         _mockCommand.SetupSet(c => c.CommandText = commandText);
@@ -84,5 +77,39 @@ public class CommandRepositoryTests
 
         // Assert
         _mockTransaction.Verify(t => t.Rollback(), Times.Once);
+    }
+
+    // 🔥 Helper method to create a fake SqlException
+    private static SqlException GenerateFakeSqlException()
+    {
+        var sqlErrorNumber = 50000; // Arbitrary SQL error number
+        var sqlError = Activator.CreateInstance(
+            typeof(SqlError),
+            BindingFlags.Instance | BindingFlags.NonPublic,
+            null,
+            new object[] { sqlErrorNumber, (byte)0, (byte)0, "", "", "", 1 },
+            null
+        );
+
+        var sqlErrorCollection = Activator.CreateInstance(
+            typeof(SqlErrorCollection),
+            BindingFlags.Instance | BindingFlags.NonPublic,
+            null,
+            null,
+            null
+        );
+
+        var addMethod = typeof(SqlErrorCollection).GetMethod("Add", BindingFlags.Instance | BindingFlags.NonPublic);
+        addMethod.Invoke(sqlErrorCollection, new[] { sqlError });
+
+        var sqlException = Activator.CreateInstance(
+            typeof(SqlException),
+            BindingFlags.Instance | BindingFlags.NonPublic,
+            null,
+            new object[] { "Mock SQL Exception", sqlErrorCollection, null, Guid.NewGuid() },
+            null
+        ) as SqlException;
+
+        return sqlException!;
     }
 }
